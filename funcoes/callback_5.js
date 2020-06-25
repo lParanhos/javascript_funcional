ba2c66dfc7522bfede8a6b189ad3c5d73f972772
lParanhos <leandro.paranhos@dextra-sw.com>
2020-06-25 10:00:39 -0300

    creating custom reduce function

--- funcoes/callback_5.js
+++ funcoes/callback_5.js
@@ -14,3 +14,23 @@ const totalGeral = carrinho
         .reduce(soma);
 
 console.log(totalGeral)
+
+Array.prototype.meuReduce = function(fn, inicial){
+    let acc = inicial;
+    for(let i = 0; i < this.length; i++){
+        if(!acc && i === 0){
+            acc = this[i];
+            continue
+        }
+        
+        acc = fn(acc, this[i], i, this)
+    }
+    return acc
+}
+
+
+const totalGeral2 = carrinho
+        .map(getTotal)
+        .meuReduce(soma);
+
+console.log(totalGeral2);
