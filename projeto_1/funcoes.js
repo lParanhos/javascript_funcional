de738755dd1ac5127a6caaa47bbd8b9006576646
lParanhos <leandro.paranhos@dextra-sw.com>
2020-07-22 08:35:31 -0300

    create reader files and filter

---
+++ projeto_1/funcoes.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+function lerDiretorio(caminho){
+    return new Promise((resolve, reject) => {
+       try {
+            let arquivos = fs.readdirSync(caminho);
+            arquivos = arquivos.map(arquivo => path.join(caminho, arquivo));
+            resolve(arquivos)
+       } catch (error) {
+            reject(error)
+       }
+    })
+}
+
+function elementosTerminadosCom(array, padrao){
+    return array.filter(elemento => elemento.endsWith(padrao))
+}
+
+
+module.exports = {
+    lerDiretorio,
+    elementosTerminadosCom
+}
