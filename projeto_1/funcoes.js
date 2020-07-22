318f2184f4b70ad3ce77e4026e2b1e343fb8fe06
lParanhos <leandro.paranhos@dextra-sw.com>
2020-07-22 08:43:47 -0300

    read file

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { resolveSoa } = require('dns');
 
 function lerDiretorio(caminho){
     return new Promise((resolve, reject) => {
@@ -17,8 +18,23 @@ function elementosTerminadosCom(array, padrao){
     return array.filter(elemento => elemento.endsWith(padrao))
 }
 
+function lerArquivo(caminho){
+    return new Promise((resolve, reject) => {
+        try {
+            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+            resolve(conteudo.toString())
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+function lerArquivos(caminhos){
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
 
 module.exports = {
     lerDiretorio,
-    elementosTerminadosCom
+    elementosTerminadosCom,
+    lerArquivos
 }
