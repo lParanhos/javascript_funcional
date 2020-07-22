ff7ab4b6f5816cc55b1fece5c90c533bd7142b4a
lParanhos <leandro.paranhos@dextra-sw.com>
2020-07-22 09:04:51 -0300

    split lines

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -5,5 +5,7 @@ const caminho = path.join(__dirname, '..', 'dados', 'legendas');
 
 fn.lerDiretorio(caminho)
     .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
-    .then(fn.lerArquivos)
+    .then(arquivosSRT =>  fn.lerArquivos(arquivosSRT))
+    .then(conteudos => conteudos.join('\n'))
+    .then(todoConteudo => todoConteudo.split('\n'))
     .then(console.log)
