2ef5c809222dc21db3fb6fc6d37387300f55f123
lParanhos <leandro.paranhos@dextra-sw.com>
2020-07-23 08:58:15 -0300

    grouping words

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -7,6 +7,18 @@ const simbolos = [
     '_', '<i>', '</i>', '\r', '[', ']'
 ]
 
+function agruparPalavras(palavras) {
+  return palavras.reduce((agrupamento, palavra) => {
+    const p = palavra.toLowerCase();
+    if(agrupamento[p]){
+        agrupamento[p] += 1
+    } else {
+        agrupamento[p] = 1
+    }
+    return agrupamento
+  }, {})
+}
+
 fn.lerDiretorio(caminho)
     .then(fn.elementosTerminadosCom('.srt'))
     .then(fn.lerArquivos)
@@ -19,4 +31,6 @@ fn.lerDiretorio(caminho)
     .then(fn.mesclarElementos)
     .then(fn.separarTextoPor(' '))
     .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeApenasNumeros)
+    .then(agruparPalavras)
     .then(console.log)
